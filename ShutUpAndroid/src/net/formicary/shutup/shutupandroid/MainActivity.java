fa98d535541d97123147cc045ad2d13533e22976
mateis <simona.matei@formicary.net>
2016-01-28 10:25:54 +0000

    HttpRequestTask class

--- ShutUpAndroid/src/net/formicary/shutup/shutupandroid/MainActivity.java
+++ ShutUpAndroid/src/net/formicary/shutup/shutupandroid/MainActivity.java
@@ -1,35 +1,69 @@
 package net.formicary.shutup.shutupandroid;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import org.springframework.http.converter.xml.SimpleXmlHttpMessageConverter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Called when the activity is first created.
+ */
 public class MainActivity extends Activity {
-  /**
-   * Called when the activity is first created.
-   */
+
+  private final String LOCALHOST_URL = "http://localhost:8083";
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.join);
+  }
 
-    final Button redButton = (Button) findViewById(R.id.join_button);
-    redButton.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View v) {
+  public void joinMeeting(View view) {
 
-       EditText host = (EditText) findViewById(R.id.host_value);
-       EditText name = (EditText) findViewById(R.id.name_value);
+    EditText host = (EditText)findViewById(R.id.host_value);
+    EditText name = (EditText)findViewById(R.id.name_value);
 
+    String hostValue = host.getText().toString();
+    String nameValue = name.getText().toString();
+
+    new HttpRequestTask(nameValue, hostValue).execute();
+  }
+
+  private class HttpRequestTask extends AsyncTask<Void, Void, ResponseEntity> {
+
+    private final String name;
+    private final String host;
+
+    public HttpRequestTask(String name, String host) {
+      super();
+      this.name = name;
+      this.host = host;
+    }
+
+    @Override
+    protected ResponseEntity doInBackground(Void... params) {
+      try {
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(new SimpleXmlHttpMessageConverter());
-
-
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+        ResponseEntity greeting = restTemplate.postForEntity(LOCALHOST_URL + "/api/connect-meeting", name, ResponseEntity.class);
+        return greeting;
+      } catch(Exception e) {
+        Log.e("MainActivity", e.getMessage(), e);
       }
-    });
 
+      return null;
+    }
+
+    @Override
+    protected void onPostExecute(ResponseEntity greeting) {
+      Intent i = new Intent(getApplicationContext(), VoteActivity.class);
+      startActivity(i);
+    }
   }
 }
---
+++ ShutUpAndroid/src/net/formicary/shutup/shutupandroid/VoteActivity.java
@@ -0,0 +1,15 @@
+package net.formicary.shutup.shutupandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VoteActivity  extends Activity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // Set View to register.xml
+    setContentView(R.layout.vote);
+  }
+
+}
