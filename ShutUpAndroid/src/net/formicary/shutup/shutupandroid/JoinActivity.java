285383f1ba6ce69516d1e6ee6a5f900901ddb4a6
mateis <simona.matei@formicary.net>
2016-01-28 11:37:05 +0000

    JoinActivity cleanup

--- ShutUpAndroid/src/net/formicary/shutup/shutupandroid/JoinActivity.java
+++ ShutUpAndroid/src/net/formicary/shutup/shutupandroid/JoinActivity.java
@@ -4,23 +4,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 import org.springframework.http.*;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-/**
- * Called when the activity is first created.
- */
-public class MainActivity extends Activity {
+public class JoinActivity extends Activity {
 
-  private final String LOCALHOST_URL = "http://10.0.2.2:8083/api/connect-meeting";
+  private final String HTTP = "http://";
+  private final String JOIN_URL = "/api/connect-meeting";
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -33,7 +32,7 @@ public class MainActivity extends Activity {
     EditText host = (EditText)findViewById(R.id.host_value);
     EditText name = (EditText)findViewById(R.id.name_value);
 
-    String hostValue = host.getText().toString();
+    String hostValue = HTTP + host.getText().toString() + JOIN_URL;
     String nameValue = name.getText().toString();
 
     new HttpRequestTask(nameValue, hostValue).execute();
@@ -51,34 +50,35 @@ public class MainActivity extends Activity {
     }
 
     @Override
+    @Nullable
     protected ResponseEntity doInBackground(Void... params) {
       try {
-        RestTemplate restTemplate = new RestTemplate();
-
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         map.add("userName", name);
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
 
-        HttpEntity<MultiValueMap<String, String>> request =  new HttpEntity<>(map, headers);
-
+        RestTemplate restTemplate = new RestTemplate();
         restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
         restTemplate.getMessageConverters().add(new FormHttpMessageConverter());
 
-        ResponseEntity greeting = restTemplate.postForEntity(LOCALHOST_URL, request, ResponseEntity.class);
-        return greeting;
+        ResponseEntity response = restTemplate.postForEntity(host, request, ResponseEntity.class);
+        return response;
       } catch(Exception e) {
-        Log.e("MainActivity", e.getMessage(), e);
+        Log.e("JoinActivity", e.getMessage(), e);
       }
-
       return null;
     }
 
     @Override
     protected void onPostExecute(ResponseEntity response) {
-      Intent i = new Intent(getApplicationContext(), VoteActivity.class);
-      startActivity(i);
+      if(response != null && response.getStatusCode().name() == "OK") {
+        Intent i = new Intent(getApplicationContext(), VoteActivity.class);
+        startActivity(i);
+      } else {
+        Toast.makeText(getApplicationContext(), "Login failed. Username already in use.", Toast.LENGTH_SHORT).show();
+      }
     }
   }
 }
