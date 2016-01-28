76bc1742370f1f40b65b2b31250b4039e4578d18
mateis <simona.matei@formicary.net>
2016-01-28 11:02:44 +0000

    Get Join meeting to work

--- ShutUpAndroid/src/net/formicary/shutup/shutupandroid/MainActivity.java
+++ ShutUpAndroid/src/net/formicary/shutup/shutupandroid/MainActivity.java
@@ -7,8 +7,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -16,7 +20,7 @@ import org.springframework.web.client.RestTemplate;
  */
 public class MainActivity extends Activity {
 
-  private final String LOCALHOST_URL = "http://localhost:8083";
+  private final String LOCALHOST_URL = "http://10.0.2.2:8083/api/connect-meeting";
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -50,8 +54,19 @@ public class MainActivity extends Activity {
     protected ResponseEntity doInBackground(Void... params) {
       try {
         RestTemplate restTemplate = new RestTemplate();
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        map.add("userName", name);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpEntity<MultiValueMap<String, String>> request =  new HttpEntity<>(map, headers);
+
         restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-        ResponseEntity greeting = restTemplate.postForEntity(LOCALHOST_URL + "/api/connect-meeting", name, ResponseEntity.class);
+        restTemplate.getMessageConverters().add(new FormHttpMessageConverter());
+
+        ResponseEntity greeting = restTemplate.postForEntity(LOCALHOST_URL, request, ResponseEntity.class);
         return greeting;
       } catch(Exception e) {
         Log.e("MainActivity", e.getMessage(), e);
@@ -61,7 +76,7 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onPostExecute(ResponseEntity greeting) {
+    protected void onPostExecute(ResponseEntity response) {
       Intent i = new Intent(getApplicationContext(), VoteActivity.class);
       startActivity(i);
     }
